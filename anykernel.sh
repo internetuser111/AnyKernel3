34189d3ac6d1f713499c1f02db2266ebea9fb30c
Cyber Knight <cyberknight755@gmail.com>
2021-10-08 16:26:09 +0800

    treewide: Imported anykernel from Sultan-kernel-floral-R-20210823.zip
    Co-authored-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -4,19 +4,16 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=DragonHeart for OnePlus 7 by cyberknight777 @ xda-developers
-do.devicecheck=0
+kernel.string=Nethunter Kernel for the Pixel 4 and Pixel 4 XL
+do.devicecheck=1
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=guacamole
-device.name2=guacamoleb
-device.name3=hotdog
-device.name4=hotdogb
-device.name5=hotdogg
-supported.versions=
-supported.patchlevels=
+device.name1=coral
+device.name2=flame
+device.name3=floral
+supported.versions=11
 '; } # end properties
 
 # shell variables
@@ -24,19 +21,11 @@ block=/dev/block/bootdevice/by-name/boot;
 is_slot_device=1;
 ramdisk_compression=auto;
 
-
 ## AnyKernel methods (DO NOT CHANGE)
 # import patching functions/variables - see for reference
 . tools/ak3-core.sh;
 
-
-## AnyKernel file attributes
-# set permissions/ownership for included ramdisk files
-set_perm_recursive 0 0 755 644 $ramdisk/*;
-set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
-
-
-## AnyKernel boot install
+## AnyKernel install
 dump_boot;
 
 write_boot;
