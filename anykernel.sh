44615fc897bd2ba6046572a23c53fc3054f8df30
Cyber Knight <cyberknight755@gmail.com>
2022-08-15 14:18:12 +0800

    AK3: Tweak for NetHunter
    - Change kernel.string.
    
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=DragonHeart for OnePlus 7 series by cyberknight777 @ xda-developers
+kernel.string=NetHunter for OnePlus 7 series by cyberknight777 @ xda-developers
 do.devicecheck=0
 do.modules=0
 do.systemless=1
